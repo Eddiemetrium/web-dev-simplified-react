fb5948458c8ee4cdb5561442f6d038e23cec6864
Eddiemetrium <eddiemitch262@gmail.com>
2023-01-11 15:11:55 -0500

    done

---
+++ src/IngredientList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Ingredient from "./Ingredient.js";
+
+function IngredientList({ ingredients }) {
+  const ingredientElements = ingredients?.map(ingredient => {
+    return <Ingredient key={ingredient.id} {...ingredient} />;
+  });
+
+  return (<div className="ingredient-grid">{ingredientElements}
+  </div>)
+}
+
+export default IngredientList;
